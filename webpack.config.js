9655af2e03cdf93508bee7c08f9a486bce50c5c6
alerkesi <alerkesi@gmail.com>
2016-01-31 23:41:07 +0300

    chore(env): pilot commit. webpack + babel + ts-loader

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    context: __dirname +'\\src\\scripts',
+    entry: './index.js',
+    output: {
+        filename: 'index.js',
+        path: __dirname + '\\release\\dev\\scripts'
+    },
+    resolve: {
+        //root: __dirname + '\\src\\scripts',
+        extensions: ['', '.js', '.jsx', '.ts', '.tsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(j|t)sx?$/,
+                loaders: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.tsx?$/,
+                loaders: [
+                    'ts-loader'
+                ]
+            }
+        ]
+    }
+};
