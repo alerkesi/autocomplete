6f8a8646960ce4548ce7de8d6bbe1d37cdfe9790
alerkesi <alerkesi@gmail.com>
2016-01-31 23:42:42 +0300

    feat(autocomplete): added Autocomplete component , html and entry

---
+++ src/scripts/components/Autocomplete.ts
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class Autocomplete extends React.Component {
+    constructor() {
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    public handleClick(event: Object): string {
+        return '';
+    }
+}
